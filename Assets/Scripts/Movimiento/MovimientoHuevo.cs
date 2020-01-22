95f1ee65bc8c5edf509cc608a403c6023a28dbd1
torcu <45042365+Exater5@users.noreply.github.com>
2020-01-22 13:47:36 +0100

    Vuelvo a implementar Animaciones de Andar
    Ahora funcionan bien dependiendo de la velocidad

--- Assets/Scripts/Movimiento/MovimientoHuevo.cs
+++ Assets/Scripts/Movimiento/MovimientoHuevo.cs
@@ -9,33 +9,53 @@ public class MovimientoHuevo : MonoBehaviour
     public float velocidad;
     public float gravedad;
     public float sensibilidad;
+    float h;
+    float v;
     float peso;
     Vector3 inputs;
     Vector3 camFward;
     Vector3 camRight;
     Vector3 movePlayer;
-
+    Animation animacion;
     // Start is called before the first frame update
     void Start()
     {
         huevo = GetComponent<CharacterController>();
+        animacion = GetComponent<Animation>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("L_XAxis_1") *-1;
-        float v = Input.GetAxis("L_YAxis_1") *-1;
+        h = Input.GetAxis("L_XAxis_1") *-1;
+        v = Input.GetAxis("L_YAxis_1") *-1;
         inputs = new Vector3(h,0,v);
         Vector3 posicionObjetivo = (h * -1 * transform.right + v * transform.forward);
         movePlayer = (posicionObjetivo * velocidad);
         SetGravity();
         movePlayer.y = peso;
-
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y - h * sensibilidad, transform.rotation.eulerAngles.z);
-        huevo.Move(movePlayer/100);
+        if (huevo.velocity.magnitude >= 0.1)
+        {
+            animacion.clip = animacion.GetClip("Corriendo");
+            animacion["Corriendo"].speed = huevo.velocity.magnitude *2;
+            animacion.Play();
+            if (Input.GetAxis("L_YAxis_1") > 0)
+            {
+                animacion["Corriendo"].speed = (huevo.velocity.magnitude * 2) *-1;
+            }
+        }
+        else
+        {
+            animacion.clip = animacion.GetClip("Parado");
+            animacion.Play();
+            print("parado");
+        }
     }
-
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y - h * sensibilidad, transform.rotation.eulerAngles.z);
+        huevo.Move(movePlayer / 100);
+    }
     void SetGravity()
     {     
         if (huevo.isGrounded)
